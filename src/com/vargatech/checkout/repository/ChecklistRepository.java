e2c78bf3176b8eb450dd54794d5558d571c0724c
vargalb <lucasvargalvl@gmail.com>
2025-12-04 20:10:34 -0300

    atualização na classe Inspecao, todos os atributos e construtores adicionados. Classe pronta

---
+++ src/com/vargatech/checkout/repository/ChecklistRepository.java
@@ -0,0 +1,12 @@
+package com.vargatech.checkout.repository;
+
+import java.util.List;
+
+public class ChecklistRepository {
+
+    private final List<String> todosOsSistemasDisponiveis;
+
+    public ChecklistRepository () {
+        this.todosOsSistemasDisponiveis = List.of("Motor", "Suspensao", "Freio", "Cambio", "Arrefecimento", "InjecaoEletronica", "ABS", "Airbag" );
+    }
+}
