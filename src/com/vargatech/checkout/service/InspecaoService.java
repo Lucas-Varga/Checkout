10d0e1645cccd1ef3f269480244be2228a6f57cf
vargalb <lucasvargalvl@gmail.com>
2025-12-12 13:09:39 -0300

    criada a branch feature/InspecaoService, e criada a dependencia da classe ChecklistRepository

---
+++ src/com/vargatech/checkout/service/InspecaoService.java
@@ -0,0 +1,14 @@
+package com.vargatech.checkout.service;
+
+import com.vargatech.checkout.repository.ChecklistRepository;
+
+public class InspecaoService {
+
+    private final InspecaoService inspecaoService;
+
+    public InspecaoService(InspecaoService inspecaoService) {
+        this.inspecaoService = inspecaoService;
+    }
+
+
+}
